b98bbab1e1e924237b0b0054347ef0d37e055f91
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-07-29 22:52:31 -0700

    define model

--- app.js
+++ app.js
@@ -5,10 +5,25 @@ const sequelize = new Sequelize({
     storage: 'movies.db'
 });
 
+//Movie model
+class Movie extends Sequelize.Model {}
+Movie.init({
+    title : Sequelize.STRING
+}, { sequelize }); //same as { sequelize : sequelize }
+
 (async () => {
+    //Sync 'Movies' table
+    await sequelize.sync({ force: true });
+
     try{
         await sequelize.authenticate();
         console.log('Connection to the database succesful!');
+        const movie = await Movie.create({
+            title: 'Toy Story'
+        });
+
+        console.log(movie.toJSON());
+
     }catch(error){
         console.error('Error connecting to the database: ', error);
     }
