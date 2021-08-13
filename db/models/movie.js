f7898ee369e10b4da1c9bd688cff9d1ffb5db83f
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-08-12 22:50:41 -0700

    further define the movies model

--- db/models/movie.js
+++ db/models/movie.js
@@ -3,7 +3,29 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
     class Movie extends Sequelize.Model {}
     Movie.init({
-        title: Sequelize.STRING,
+        //Set custom primary key column
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        runtime: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+         },
+        releaseDate: { 
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+        },
+        isAvailableOnVHS: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false, //set default value to false
+         },
     }, { sequelize });
 
     return Movie;
