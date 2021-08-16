586574fd8caf270ce56e5a48b753ab1c0eff9bf2
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-08-16 15:47:13 -0700

    add validators for movie model

--- db/models/movie.js
+++ db/models/movie.js
@@ -12,14 +12,40 @@ module.exports = (sequelize) => {
         title: {
             type: Sequelize.STRING,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "title"',
+                },
+                notEmpty: {
+                    msg: 'Please provide a value for "title"',
+                },
+            },
         },
         runtime: {
             type: Sequelize.INTEGER,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "runtime"',
+                },
+                min: {
+                    args: 1,
+                    msg: 'Please provide a value greater than "0" for "runtime"',
+                },
+            },
          },
         releaseDate: { 
             type: Sequelize.DATEONLY,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please provide a value for "realeaseDate"',
+                },
+                isAfter: {
+                    args: '1895-12-27',
+                    msg: 'Please provide a value on or after "1895-12-28" for "releaseDate"',
+                },
+            },
         },
         isAvailableOnVHS: {
             type: Sequelize.BOOLEAN,
